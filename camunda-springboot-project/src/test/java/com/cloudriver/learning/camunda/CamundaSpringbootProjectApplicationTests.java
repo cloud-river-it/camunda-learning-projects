645a720f356fff291ffa3c743aaa5d5494412deb
mounir <mounir.mahmoud@gmail.com>
2020-07-11 00:08:53 -0400

    add first child projet

---
+++ camunda-springboot-project/src/test/java/com/cloudriver/learning/camunda/CamundaSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudriver.learning.camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
